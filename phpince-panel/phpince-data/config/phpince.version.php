2bdda9f0e745fa75894a3a95f973fba02e0678dd
Dominik Hulla <dh@bladrion.com>
2013-09-07 16:46:15 +0200

    Updated version to 3.0.4

--- phpince-panel/phpince-data/config/phpince.version.php
+++ phpince-panel/phpince-data/config/phpince.version.php
@@ -23,7 +23,7 @@
 +----------------------------------------------------------------------*/
 $PHPINCE_VERSION = array(
 	"CORE" => "Bladrion UniSC 2.0",
-	"SYSTEM" => "3.0.3",
+	"SYSTEM" => "3.0.4",
 	"SUBVERSION" => "",
 	"EDITOR" => array(
 		"tinymce" => "TinyMCE 4.0.5",
