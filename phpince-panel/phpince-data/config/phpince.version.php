3ee8f8e11f23a3dde69dba99724d3af94fb54d6f
Dominik Hulla <dh@bladrion.com>
2014-01-20 09:25:20 +0100

    Update editor TinyMCE 4.0.12

--- phpince-panel/phpince-data/config/phpince.version.php
+++ phpince-panel/phpince-data/config/phpince.version.php
@@ -26,7 +26,7 @@ $PHPINCE_VERSION = array(
 	"SYSTEM" => "3.2.4",
 	"SUBVERSION" => "",
 	"EDITOR" => array(
-		"tinymce" => "TinyMCE 4.0.11",
+		"tinymce" => "TinyMCE 4.0.12",
 		"ckeditor" => "CKEditor 4.3"
 	)
 );
