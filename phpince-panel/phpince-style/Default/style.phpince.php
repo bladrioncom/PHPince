76537f4f4146f064639108650b5cd89baa878d79
dominikhulla <dh@bladrion.com>
2014-02-04 11:24:53 +0100

    New Default template

--- phpince-panel/phpince-style/Default/style.phpince.php
+++ phpince-panel/phpince-style/Default/style.phpince.php
@@ -1,11 +1,11 @@
 <?php
 $PHPINCE_STYLE = array(
-	"topic_style" => "<h1>{PHPINCE_TITLE} | {PHPINCE_DATE} - {PHPINCE_TIME}</h1><p>{PHPINCE_TEXT}</p><hr>",
-	"topic_style_active" => "<h1>{PHPINCE_TITLE}</h1><p>{PHPINCE_TEXT}</p>",
+	"topic_style" => "<div class=\"blog-post\"><h2 class=\"blog-post-title\">{PHPINCE_TITLE}</h2><p class=\"blog-post-meta\">{PHPINCE_DATE} - {PHPINCE_TIME}</p>{PHPINCE_TEXT}</div>",
+	"topic_style_active" => "<div class=\"blog-post\"><h2 class=\"blog-post-title\">{PHPINCE_TITLE}</h2><p class=\"blog-post-meta\">{PHPINCE_DATE} - {PHPINCE_TIME}</p>{PHPINCE_TEXT}</div>",
 	"topic_error" => "No topic writed",
-	"topic_notfound" => "Sorry, topic not found",
-	"page_style" => "<h1>{PHPINCE_TITLE}</h1><p>{PHPINCE_TEXT}</p>",
-	"page_notfound" => "Sorry, page not found",
-	"plugin_notfound" => "Plugin not found",
+	"topic_notfound" => "<div class=\"blog-post\"><h2 class=\"blog-post-title\">404 - Topic not found</h2>Sorry, topic not found</div>",
+	"page_style" => "<div class=\"blog-post\"><h2 class=\"blog-post-title\">{PHPINCE_TITLE}</h2>{PHPINCE_TEXT}</div>",
+	"page_notfound" => "<div class=\"blog-post\"><h2 class=\"blog-post-title\">404 - Page not found</h2>Sorry, page not found</div>",
+	"plugin_notfound" => "<div class=\"blog-post\"><h2 class=\"blog-post-title\">404 - Plugin not found</h2>Sorry, plugin not found</div>",
 );
 ?>
